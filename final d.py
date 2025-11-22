70a6116d515c47fb2885f85cc8090d2eeda850b5
ssaurabh5135 <ssaurabh5135@gmail.com>
2025-11-22 10:29:50 +0530

    Update final d.py

--- final d.py
+++ final d.py
@@ -9,7 +9,7 @@ from google.oauth2.service_account import Credentials
 
 st.set_page_config(page_title="Factory Dashboard (Exact Layout)", layout="wide")
 
-IMAGE_PATH = "winter.jpg"
+IMAGE_PATH = "snow.jpg"
 SPREADSHEET_ID = "168UoOWdTfOBxBvy_4QGymfiIRimSO2OoJdnzBDRPLvk"
 DASHBOARD_SHEET = "Dashboard"
 SALES_REPORT_SHEET = "Sales Report"
@@ -975,4 +975,5 @@ st.components.v1.html(html_template, height=770, scrolling=True)
 # """
 
 # st.components.v1.html(html_template, height=770, scrolling=True)
+
 
